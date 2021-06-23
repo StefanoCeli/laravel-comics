91b78dd66c5072f418c4f737b2dcb0039b53f81b
StefanoCeli <stefanopsn91@gmail.com>
2021-06-23 02:00:48 +0200

    fine esercizio

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,103 @@
+<footer>
+    <section class="commercial">
+        <div class="wrap">
+          
+          <div>
+              <a href="#">
+                  <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="digital-comics">
+                  DIGITAL COMICS
+              </a>
+          </div>
+          <div>
+              <a href="#">
+                  <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="dc-merchandise">
+                  DC MERCHANDISE
+              </a>
+          </div>
+          <div>
+              <a href="#">
+                  <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="subscriptions">
+                  SUBSCRIPTIONS
+              </a>
+          </div>
+          <div>
+              <a href="#">
+                  <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="shop-locator">
+                  COMIC SHOP LOCATOR
+              </a>
+          </div>
+          <div>
+              <a href="#">
+                  <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="dc-power-visa">
+                  DC POWER VISA
+              </a>
+          </div>
+          
+           
+        </div>
+    </section>
+    <section class="footer-top">
+        <div class="wrap">
+            <div class="menu-footer">
+                <div>
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                    <h3>SHOP</h3>
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>DC</h3>
+                    <ul>
+                      <li><a href="#">Terms Of Use</a></li>
+                      <li><a href="#">Privacy policy (New)</a></li>
+                      <li><a href="#">Ad Choices</a></li>
+                      <li><a href="#">Advertising</a></li>
+                      <li><a href="#">Jobs</a></li>
+                      <li><a href="#">Subscriptions</a></li>
+                      <li><a href="#">Talent Workshops</a></li>
+                      <li><a href="#">CPSC Certificates</a></li>
+                      <li><a href="#">Ratings</a></li>
+                      <li><a href="#">Shop Help</a></li>
+                      <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>SITES</h3>
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="footer-bot">
+
+        <div class="wrap">
+            <button>SIGN-UP NOW!</button>
+            <div>
+                <h2>FOLLOW US</h2>
+                <a href="#"><img src="{{asset('../img/footer-facebook.png')}}" alt=""></a>
+                <a href="#"><img src="{{asset('../img/footer-twitter.png')}}" alt=""></a>
+                <a href="#"><img src="{{asset('../img/footer-youtube.png')}}" alt=""></a>
+                <a href="#"><img src="{{asset('../img/footer-pinterest.png')}}" alt=""></a>
+                <a href="#"><img src="{{asset('../img/footer-periscope.png')}}" alt=""></a>
+            </div>
+        </div>
+
+    </section>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+    <nav class="wrap">  
+        <img src="{{ asset('img/dc-logo.png') }}" alt="">
+        <ul>
+            <li><a href="{{ route('characters')}}" class="{{ (request()-> is ('characters')) ? 'active' : ' ' }}">CHARACTERS</a></li>
+            <li><a href="{{ route('comics')}}" class="{{ (request()-> is ('/')) ? 'active' : ' ' }}">COMICS</a></li>
+            <li><a href="{{ route('movies')}}" class="{{ (request()-> is ('movies')) ? 'active' : ' ' }}">MOVIES</a></li>
+            <li><a href="{{ route('tv')}}" class="{{ (request()-> is ('tv')) ? 'active' : ' ' }}">TV</a></li>
+            <li><a href="{{ route('games')}}" class="{{ (request()-> is ('games')) ? 'active' : ' ' }}">GAMES</a></li>
+            <li><a href="{{ route('collectibles')}}" class="{{ (request()-> is ('collectibles')) ? 'active ' : ' ' }}">COLLECTIBLES</a></li>
+            <li><a href="{{ route('videos')}}" class="{{ (request()-> is ('videos')) ? 'active' : ' ' }}">VIDEOS</a></li>
+            <li><a href="{{ route('fans')}}" class="{{ (request()-> is ('fans')) ? 'active' : ' ' }}">FANS</a></li>
+            <li><a href="{{ route('news')}}" class="{{ (request()-> is ('news')) ? 'active' : ' ' }}">NEWS</a></li>
+            <li><a href="{{ route('shop')}}" class="{{ (request()-> is ('shop')) ? 'active' : ' ' }}">SHOP</a></li>
+        </ul>   
+    </nav>
+    <div class="_jumbotron"></div>
+</header>
