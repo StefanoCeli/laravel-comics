4df3ba1133e8c2b9abf524be2bb0601ada7c4d0d
StefanoCeli <stefanopsn91@gmail.com>
2021-06-23 02:04:38 +0200

    fixato link

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,7 +7,7 @@
             <li><a href="{{ route('movies')}}" class="{{ (request()-> is ('movies')) ? 'active' : ' ' }}">MOVIES</a></li>
             <li><a href="{{ route('tv')}}" class="{{ (request()-> is ('tv')) ? 'active' : ' ' }}">TV</a></li>
             <li><a href="{{ route('games')}}" class="{{ (request()-> is ('games')) ? 'active' : ' ' }}">GAMES</a></li>
-            <li><a href="{{ route('collectibles')}}" class="{{ (request()-> is ('collectibles')) ? 'active ' : ' ' }}">COLLECTIBLES</a></li>
+            <li><a href="{{ route('collectibles')}}" class="{{ (request()-> is ('collectibles')) ? 'active' : ' ' }}">COLLECTIBLES</a></li>
             <li><a href="{{ route('videos')}}" class="{{ (request()-> is ('videos')) ? 'active' : ' ' }}">VIDEOS</a></li>
             <li><a href="{{ route('fans')}}" class="{{ (request()-> is ('fans')) ? 'active' : ' ' }}">FANS</a></li>
             <li><a href="{{ route('news')}}" class="{{ (request()-> is ('news')) ? 'active' : ' ' }}">NEWS</a></li>
